792c1f2f83229c783c115247df398fd619b87865
anushhhka <anushkasingh88888@gmail.com>
2024-10-03 13:39:45 +0530

    second commit

--- android/app/src/main/kotlin/com/example/mazdoor_user/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/mazdoor_user/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.mazdoor_user
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterActivity // Correct import
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    // You can add any additional configuration or methods here if needed
+}
